83970bec8b6c161bbbc7676d199c4d223cae0beb
nenshoukei <mokkosu.nico@gmail.com>
2026-03-23 00:12:27 +0900

    Add Planet Paracelsin support

---
+++ scripts/prototype/mods/Paracelsin.lua
@@ -0,0 +1,8 @@
+--- 🌐 Planet Paracelsin by Andreus
+--- https://mods.factorio.com/mod/Paracelsin
+
+local PrototypeColorRegistry = require("scripts.prototype.prototype-color-registry")
+
+if mods["Paracelsin"] then
+  PrototypeColorRegistry.set("galvanization-science-pack", { 0.71, 0.35, 0.13 })
+end
