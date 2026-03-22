627030fda16819683cfc8ec39f0831607dbdfe78
nenshoukei <mokkosu.nico@gmail.com>
2026-03-22 20:17:16 +0900

    Add Secretas&Frozeta support

---
+++ scripts/prototype/mods/secretas.lua
@@ -0,0 +1,8 @@
+--- 🌐Secretas&Frozeta by Zach Kolansky
+--- https://mods.factorio.com/mod/secretas
+
+local PrototypeColorRegistry = require("scripts.prototype.prototype-color-registry")
+
+if mods["secretas"] then
+  PrototypeColorRegistry.set("golden-science-pack", { 0.97, 0.75, 0.46 })
+end
