c2dae8f364779771034e9f7c385b6cdda436d1e4
nenshoukei <mokkosu.nico@gmail.com>
2026-04-05 20:57:37 +0900

    Fix Big Lab support to apply the same on_animation modifications as for the vanilla labs Fixes #52

--- scripts/prototype/mods/BigLabFork.lua
+++ scripts/prototype/mods/BigLabFork.lua
@@ -4,9 +4,16 @@
 if not mods["BigLabFork"] then return {} end
 
 local PrototypeLabRegistry = require("scripts.prototype.prototype-lab-registry")
+local AnimationHelpers = require("scripts.prototype.animation-helpers")
 
 return {
   on_data = function ()
     PrototypeLabRegistry.register("big-lab")
+  end,
+
+  on_data_final_fixes = function ()
+    AnimationHelpers.modify_on_animation("big-lab", function (modifier)
+      modifier:apply_lab_modifications()
+    end)
   end,
 }
